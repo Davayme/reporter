7f825323a05ab985872c2ac53f55779802f80eb7
Edder <enaranjo5489@uta.edu.ec>
2024-05-21 21:14:52 -0500

    MODIFICACION - Se modifico el UserModule y AppModule segun la logica requirida por el modulo user

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AdminModule } from './admin/admin.module';
-import { UserModule } from './user/user.module';
+import { UserModule } from './user/UserModule';
 
 
 @Module({
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,18 +1,3 @@
-import { Module } from '@nestjs/common';
-import { UserController } from './infrastructure/controllers/user.controller';
-import { PrismaUserRepository } from './infrastructure/repositories/prisma-user.repository';
-import { UpdateUserService } from './application/services/update-user.service';
 
-@Module({
-  controllers: [UserController],
-  providers: [
-    PrismaUserRepository,
-    {
-      provide: 'UserRepository',
-      useExisting: PrismaUserRepository,
-    },
-    UpdateUserService
-  ],
-  exports: [UpdateUserService],
-})
-export class UserModule {}
+
+
