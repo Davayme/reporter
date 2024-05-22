e91e130bc17b98a6232b0a257ee416158244459e
Edder <enaranjo5489@uta.edu.ec>
2024-05-21 21:31:47 -0500

    MODIFICACION - Se modifico la ruta y se mejoro el UserModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AdminModule } from './admin/admin.module';
-import { UserModule } from './user/UserModule';
-
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [AdminModule, UserModule],
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,3 +1,31 @@
 
+import { Module } from '@nestjs/common';
+import { PrismaUserRepository } from './infrastructure/repositories/prisma-user.repository';
+import { UpdateUserService } from './application/services/update-user.service';
+import { UserController } from './infrastructure/controllers/user.controller';
 
 
+
+@Module({
+  controllers: [UserController],
+
+  providers: [
+
+    PrismaUserRepository,
+    {
+
+      provide: 'UserRepository',
+
+      useExisting: PrismaUserRepository,
+
+    },
+
+    UpdateUserService
+
+    
+  ],
+  exports: [UpdateUserService],
+
+}
+)
+export class UserModule {}
