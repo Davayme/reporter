fa59cf0ba97605349cceb3cf98f4615c31d1e30c
David Ayme <dayme1937@uta.edu.ec>
2024-05-23 07:42:39 -0500

    Agregado el cifrado de contraseñas

--- src/admin/application/services/create-user.service.ts
+++ src/admin/application/services/create-user.service.ts
@@ -1,18 +1,25 @@
 // src/user/application/services/create-user.service.ts
 import { Inject, Injectable } from '@nestjs/common';
 import { AdminRepository } from 'src/admin/domain/repositories/admin.repository';
-import { CreateUserCommand} from '../commands/admin.command';
+import { CreateUserCommand } from '../commands/admin.command';
 import { User } from '@prisma/client';
-
+import * as bcrypt from 'bcrypt';
 @Injectable()
 export class CreateUserService {
   constructor(
     @Inject('AdminRepository') private readonly adminRepository: AdminRepository
-  ) {}
+  ) { }
 
   async execute(command: CreateUserCommand): Promise<User> {
     const { username, password, email } = command.userDto;
-    const newUser =await this.adminRepository.create({ username, password, email, roleId:2, statusActive: true });
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await this.adminRepository.create({
+      username,
+      password: hashedPassword,
+      email,
+      roleId: 2,
+      statusActive: true
+    });
     return newUser;
   }
 }
