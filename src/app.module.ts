6059e847e9eefadb4dac35a082a5b384fe64c570
WADEPAR6 <kparedes9203@uta.edu.ec>
2024-05-22 22:55:24 -0500

    MODIFICACION - Se añadio en el modulo del backend la logica del JWT y de misma manera el modulo de authentificacion

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AdminModule } from './admin/admin.module';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import jwtConfig from './common/config/jwt.config';
+import { PrismaModule } from './common/prisma/prisma.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AdminModule],
+  imports: [
+    ConfigModule.forRoot({
+      load: [jwtConfig],
+      isGlobal: true,
+    }),
+    PrismaModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('jwt.secret'),
+        signOptions: configService.get('jwt.signOptions'),
+      }),
+      inject: [ConfigService],
+    }),
+    AuthModule,
+    AdminModule],
   controllers: [],
   providers: [],
 })
