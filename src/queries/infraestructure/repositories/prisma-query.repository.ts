535f257f0b12f10b60e165e9a30d30f5e676e734
David Ayme <dayme1937@uta.edu.ec>
2024-06-06 19:28:49 -0500

    Modificado el primsa repository para controlar excepciones

--- src/queries/infraestructure/repositories/prisma-query.repository.ts
+++ src/queries/infraestructure/repositories/prisma-query.repository.ts
@@ -1,5 +1,6 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { PrismaClient, Query } from '@prisma/client';
+import { PrismaClientKnownRequestError, PrismaClientValidationError } from '@prisma/client/runtime/library';
 import { QueryRepository } from 'src/queries/domain/repositories/query.repository';
 
 @Injectable()
@@ -21,42 +22,87 @@ export class PrismaQueryRepository implements QueryRepository {
   }
 
   async create(query: Omit<Query, 'id_querie'>): Promise<Query> {
-    const serverExists = await this.prisma.server.findUnique({
-      where: { id: query.id_server },
-    });
+    try {
+      const serverExists = await this.prisma.server.findUnique({
+        where: { id: query.id_server },
+      });
 
-    if (!serverExists) {
-      throw new NotFoundException(`Server with id ${query.id_server} not found`);
+      if (!serverExists) {
+        throw new NotFoundException(`Server with id ${query.id_server} not found`);
+      }
+
+      return await this.prisma.query.create({
+        data: query
+      });
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        // Manejar errores específicos de Prisma, como violaciones de restricciones únicas
+        if (error.code === 'P2002') {
+          throw new BadRequestException('A query with the specified details already exists.');
+        }
+        throw new BadRequestException(error.message);
+      } else if (error instanceof PrismaClientValidationError) {
+        // Manejar errores de validación de Prisma
+        throw new BadRequestException('Validation error in Prisma operation.');
+      } else {
+        // Manejar cualquier otro tipo de error
+        throw new InternalServerErrorException('An unexpected error occurred.');
+      }
     }
-
-    return this.prisma.query.create({
-      data: query
-    });
   }
 
   async update(id: number, query: Partial<Query>): Promise<Query> {
-    const existingQuery = await this.prisma.query.findUnique({ where: { id_querie: id } });
+    try {
+      const existingQuery = await this.prisma.query.findUnique({ where: { id_querie: id } });
 
-    if (!existingQuery) {
-      throw new NotFoundException('Query not found');
+      if (!existingQuery) {
+        throw new NotFoundException('Query not found');
+      }
+
+      return await this.prisma.query.update({
+        where: { id_querie: id },
+        data: query
+      });
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        // Manejar errores específicos de Prisma, como violaciones de restricciones únicas
+        if (error.code === 'P2002') {
+          throw new BadRequestException('The update operation violates a unique constraint.');
+        }
+        throw new BadRequestException(error.message);
+      } else if (error instanceof PrismaClientValidationError) {
+        // Manejar errores de validación de Prisma
+        throw new BadRequestException('Validation error in Prisma operation.');
+      } else {
+        // Manejar cualquier otro tipo de error
+        throw new InternalServerErrorException('An unexpected error occurred.');
+      }
     }
-
-    return this.prisma.query.update({
-      where: { id_querie: id },
-      data: query
-    });
   }
 
   async delete(id: number): Promise<Query> {
-    const existingQuery = await this.prisma.query.findUnique({ where: { id_querie: id } });
+    try {
+      const existingQuery = await this.prisma.query.findUnique({ where: { id_querie: id } });
 
-    if (!existingQuery) {
-      throw new NotFoundException('Query not found');
+      if (!existingQuery) {
+        throw new NotFoundException('Query not found');
+      }
+
+      return await this.prisma.query.update({
+        where: { id_querie: id },
+        data: { statusActive: false }
+      });
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        // Manejar errores específicos de Prisma
+        throw new BadRequestException(error.message);
+      } else if (error instanceof PrismaClientValidationError) {
+        // Manejar errores de validación de Prisma
+        throw new BadRequestException('Validation error in Prisma operation.');
+      } else {
+        // Manejar cualquier otro tipo de error
+        throw new InternalServerErrorException('An unexpected error occurred.');
+      }
     }
-
-    return this.prisma.query.update({
-      where: { id_querie: id },
-      data: { statusActive: false }
-    });
   }
 }
