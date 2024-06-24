c0da5df87c74b42db502c72f891b7c6207029e8c
David Ayme <dayme1937@uta.edu.ec>
2024-06-24 10:54:59 -0500

    Implementado el patron Factory Method en la logica de conexion de bases de datos

--- src/generate-templates/infraestructure/knex-service/knex.service.ts
+++ src/generate-templates/infraestructure/knex-service/knex.service.ts
@@ -1,33 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { Server } from '@prisma/client';
 import { DatabaseInterface } from './IDatabase';
-import { MysqlDatabase } from './MysqlDatabase';
-import { PgDatabase } from './PgDatabase';
-import { OracleDatabase } from './OracleDatabase';
-import { SqlServerDatabase } from './SqlServerDatabase';
-
+import { DatabaseFactory } from './DatabaseFactory';
 @Injectable()
 export class KnexService {
   private db: DatabaseInterface;
 
   async connect(server: Server): Promise<void> {
-    switch (server.type_bd) {
-      case 'mysql':
-        this.db = new MysqlDatabase();
-        break;
-      case 'pg':
-        this.db = new PgDatabase();
-        break;
-      case 'sqlserver':
-        this.db = new SqlServerDatabase();
-        break;
-      case 'oracle':
-        this.db = new OracleDatabase();
-        break;
-      default:
-        throw new Error('Unsupported database type');
-    }
-
+    this.db = DatabaseFactory.createDatabase(server);
     await this.db.connect(server);
   }
 
